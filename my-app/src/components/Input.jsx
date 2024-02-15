5f4cc09403d0d4810457cadb216e883b79f82e4a
Ayush Gupta <guptaayush212004@gmail.com>
2024-02-15 23:43:21 +0530

    updates

--- my-app/src/components/Input.jsx
+++ my-app/src/components/Input.jsx
@@ -1,3 +1,40 @@
-export default function Input(){
-    return <h1>Input</h1>
+import { useState } from "react"
+
+export default function Input(props){
+    let sampleData={
+        name:"",
+        id:"",
+        address:"",
+        url:""
+    }
+
+    const [data, setData] = useState(sampleData)
+
+    function saveName(event){
+        setData({...data, name:event.target.value})
+    }
+
+    function saveID(event){
+        setData({...data, id:event.target.value})
+    }
+
+    function saveURL(event){
+        setData({...data, url:event.target.value})
+    }
+
+    function saveAddress(event){
+        setData({...data, address:event.target.value})
+    }
+
+    return (
+        <>
+            <div className="d-flex flex-column">
+                <input type="text" placeholder="Name" onChange={saveName}/>
+                <input type="text" placeholder="ID"  onChange={saveID}/>
+                <input type="text" placeholder="URL"  onChange={saveURL}/>
+                <input type="text" placeholder="address" onChange={saveAddress}/>
+                <button className="btn btn-primary" onClick={()=>props.addContact(data)} >Add Contact</button>
+            </div>
+        </>
+    )
 }
--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -2,43 +2,56 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Output from './components/Output'
 import Input from './components/Input'
+import { useState } from 'react'
 
-let emps = [
-    {
-        name:"Ayush",
-        id:12,
-        address:"Derbassi",
-        url:""
-    },
-    {
-        name:"Sonu",
-        id:123,
-        address:"Banur",
-        url:""
-    },
-    {
-        name:"Ashwini",
-        id:1245,
-        address:"Rajpura",
-        url:""
+function App(){
+    let emps = [
+        {
+            name:"Ayush",
+            id:12,
+            address:"Derbassi",
+            url:"",
+        },
+        {
+            name:"Sonu",
+            id:123,
+            address:"Banur",
+            url:"",
+        },
+        {
+            name:"Ashwini",
+            id:1245,
+            address:"Rajpura",
+            url:"",
+        },
+    ]
+    
+    const [emp, setEmp] = useState(emps)
+
+    function addContact(data){
+        setEmp([...emp, data])
     }
-]
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <div>
+    return (
+        <div>
         <div className="container-fluid">
             <div className="row">
                 <div className="col-lg-8">
                     {
-                        emps.map((object)=>{
+                        emp.map((object)=>{
                             return <Output name={object.name} id={object.id} address={object.address} url={""} />
                         })
                     }
                 </div>
                 <div className="col-lg-4">
-                    <Input/>
+                    <Input addContact={addContact} />
                 </div>
             </div>
         </div>
     </div>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
 )
